5906063e5b1da19195be163ca9cfc9d50c198428
Felipe Marlon <mr.sheldonvs@gmail.com>
2021-02-01 22:10:05 -0300

    file upload

--- bandsmanage/models.py
+++ bandsmanage/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 from uuid import uuid4
 
+
+def upload_art_band(instance,filename):
+    return f"{instance.id_band}-{filename}"
+
+
 # Create your models here
+
+
+
 
 class Bands(models.Model):
     id_band = models.UUIDField(primary_key=True, default=uuid4, editable=False)
@@ -9,5 +17,6 @@ class Bands(models.Model):
     genre = models.CharField(max_length=255)
     sub_genre = models.CharField(max_length=255)
     release_year = models.IntegerField()
+    art = models.ImageField(upload_to=upload_art_band, blank=True, null=True)
     created_at = models.DateField(auto_now_add=True)
 
