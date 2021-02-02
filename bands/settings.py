65deb9032b874e311c8dc0e23999f4c99aa620fe
Felipe Marlon <mr.sheldonvs@gmail.com>
2021-02-01 22:00:51 -0300

    upload de arquivo

--- bands/settings.py
+++ bands/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'p%t6sl$1b@rzpas8lcj2+qmx)ig7lodi4cfs0h&!^+z1jq-*5s'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -38,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bandsmanage',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'bands.urls'
@@ -120,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
--- bands/urls.py
+++ bands/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework import routers
+from django.conf.urls.static import static
+from django.conf import settings
 from bandsmanage.api import viewsets as BandsmanageViewset
 
 
@@ -12,4 +14,4 @@ route.register(r'bands', BandsmanageViewset.BandsmanageViewset, basename="bands"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(route.urls))
-]
+]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
